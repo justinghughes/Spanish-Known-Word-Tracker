7188516be0fea8e9b75fb5f26a8fbfb6375c4a49
Justin H <justinghughes134@gmail.com>
2022-10-19 11:47:43 -0400

    Make click event output innerText of target

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ unknownWords.forEach((unknown) => {
     clickableWord.classList.add(unknown);
     clickableWord.textContent = unknown;
     unknownContainer.appendChild(clickableWord);
+
+    // This event below accesses the button(word) that I click by using e function to track my mouse then uses splice to remove 1 element by inserting the number '1' in the second parameter then inserting e.target in the first parameter to determine where I want that 1 element to be removed.
+
     clickableWord.addEventListener('click', function(e) {
-        const knownWords = unknownWords.splice(e.target, 1);
-        console.log(knownWords)
+        knownPara.textContent = e.target.innerText;
+        knownContainer.appendChild(knownPara);
+
+        console.log(e.target.innerText)
+        console.log(e)
+        // console.log(e.target);
+        // console.log(e)
         // knownPara.textContent = knownWords;
         // knownContainer.appendChild(knownPara);
     });
