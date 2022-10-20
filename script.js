a1492ab39bd83d5c9b7e6b24682d83039af1c95c
Justin H <justinghughes134@gmail.com>
2022-10-20 10:14:56 -0400

    Make knownWords array add clicked word

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-let unknownWords = ['Los', 'vendimiadores', 'lo', 'miraban', 'de', 'reojo', 'en', 'un', 'mal', 'disimulado', 'desprecio']
 const unknownContainer = document.querySelector('#unknown-container');
 const knownContainer = document.querySelector('#known-container');
-const knownPara = document.querySelector('#known-para');
+let unknownWords = ['Hola', 'me', 'llamaste', 'anoche'];
+let knownWords = [];
 
-
-// for (let i = 0; i < unknownWords.length; i++) {
-//     const clickableWords = document.createElement('button');
-// }
-
-unknownWords.forEach((unknown) => {
-    const clickableWord = document.createElement('button');
-    clickableWord.classList.add(unknown);
-    clickableWord.textContent = unknown;
+unknownWords.forEach((word) => {
+    let clickableWord = document.createElement('button');
+    clickableWord.textContent = word;
     unknownContainer.appendChild(clickableWord);
-
-    // This event below accesses the button(word) that I click by using e function to track my mouse then uses splice to remove 1 element by inserting the number '1' in the second parameter then inserting e.target in the first parameter to determine where I want that 1 element to be removed.
-
-    clickableWord.addEventListener('click', function(e) {
-        knownPara.textContent = e.target.innerText;
-        knownContainer.appendChild(knownPara);
-
-        console.log(e.target.innerText)
-        console.log(e)
-        // console.log(e.target);
-        // console.log(e)
-        // knownPara.textContent = knownWords;
-        // knownContainer.appendChild(knownPara);
-    });
+    clickableWord.addEventListener('click', function() {
+        // console.log(unknownWords.indexOf(word));
+        let unknownIndex = unknownWords.indexOf(word);
+        knownWords.push(word);
+        // console.log(unknownWords);
+        console.log(knownWords);
+    })
 });
 
-// clickBtn() {
-//     return console.log('hey')
-// }
 
-// clickableWord.addEventListener('click', () => {
-//     unknownWords.splice(0,1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// unknownWords.forEach((unknown) => {
+//     const clickableWord = document.createElement('button');
+//     clickableWord.classList.add(unknown);
+//     clickableWord.textContent = unknown;
+//     unknownContainer.appendChild(clickableWord);
+//     clickableWord.addEventListener('click', function(e) {
+//         console.log(Array.prototype.findIndex())
+//     })
 // });
 
 
-// document.addEventListener('contextmenu', function (e) {
-//     console.log("You tried to open context menu");
-// })
+
+
+
