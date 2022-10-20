5ea9ab90a771a3fb5c55dd8aecea0aa8ef199cc0
Justin H <justinghughes134@gmail.com>
2022-10-20 11:50:53 -0400

    Tweak style properties

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const unknownContainer = document.querySelector('#unknown-container');
 const knownContainer = document.querySelector('#known-container');
+const unknownPara = document.querySelector('#unknown-para');
 const knownPara = document.querySelector('#known-para');
-let unknownWords = ['Hola', 'me', 'llamaste', 'anoche'];
+let str = 'Un hombre de 43 años también está detenido, acusado de ayudar a ocultar el cuerpo de la niña, según el comunicado de la Fiscalía. El ministro de Justicia, Eric Dupond-Moretti, declaró el martes ante la Asamblea Nacional que la mujer detenida recibió una orden de abandonar Francia el 22 de agosto. La sospechosa era desconocida para la policía, añadió, pero ella misma era víctima de la violencia. Este asesino en serie y caníbal es tan pura maldad que retiraron de la venta su disfraz para Halloween. Entró legalmente al país en 2016 tras obtener una visa de estudiante que había expirado desde entonces, dijo Veran. En agosto, cuando aterrizó en el aeropuerto parisino de Orly, las autoridades se percataron de su estatus migratorio y le dieron la orden de abandonar el país, pero la dejaron entrar, explicó el portavoz. Los políticos de extrema derecha han arremetido contra el Gobierno centrista del presidente francés, Emmanuel Macron, y han culpado a sus políticas de inmigración de la muerte de la niña. Una vez más, el sospechoso de esta barbarie no debería haber estado en Francia. A qué esperan para actuar para que se detenga de una vez esta inmigración ilegal fuera de control?, escribió el martes en la red social Twitter Marine Le Pen, que este año se presentó sin éxito a las elecciones presidenciales por tercera vez.'
+
+let unknownWords = str.split(' ');
+console.log(unknownWords)
+
+// let unknownWords = ['Hola', 'me', 'llamaste', 'anoche'];
+
 let knownWords = [];
 console.log("Unknown Words: " + unknownWords);
 console.log("Known Words: " + knownWords);
 
 unknownWords.forEach((word) => {
+
     let clickableWord = document.createElement('button');
     clickableWord.textContent = word;
-    unknownContainer.appendChild(clickableWord);
+    unknownPara.appendChild(clickableWord)
+    unknownContainer.appendChild(unknownPara);
     clickableWord.addEventListener('click', function() {
-        // console.log(unknownWords.indexOf(word));
-        // let unknownIndex = unknownWords.indexOf(word);
-        // console.log(unknownIndex)
         clickableWord.classList.add('clicked-btn');
         clickableWord.disabled = true;
         let idx = unknownWords.indexOf(word);
@@ -23,7 +29,6 @@ unknownWords.forEach((word) => {
         console.log("Known Words: " + knownWords);
         knownPara.textContent = knownWords;
         knownContainer.appendChild(knownPara);
-        
     })});
 
 
